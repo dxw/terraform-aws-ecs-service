540c963d89c606757c72ec9d3a527052ab6f8a07
Chris Wright <chris@dxw.com>
2021-05-11 18:47:21 +0100

    Terraform 0.15.* updates

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "service_url" {
   value = var.enable_lb ? lower(join(",", formatlist(
       "%s://%s:%s",
       aws_alb_listener.main.*.protocol,
-      element(concat(aws_alb.main.*.dns_name, list("")), 0),
+      element(concat(aws_alb.main.*.dns_name, tolist([""])), 0),
       aws_alb_listener.main.*.port
   ))) : ""
 }
@@ -12,19 +12,19 @@ output "service_url" {
 output "lb_dns_name" {
   description = "Loadbalancer DNS Name"
 
-  value = var.enable_lb ? element(concat(aws_alb.main.*.dns_name, list("")), 0) : ""
+  value = var.enable_lb ? element(concat(aws_alb.main.*.dns_name, tolist([""])), 0) : ""
 }
 
 output "lb_arn" {
   description = "Loadbalancer ARN"
 
-  value = var.enable_lb ? element(concat(aws_alb.main.*.arn, list("")), 0) : ""
+  value = var.enable_lb ? element(concat(aws_alb.main.*.arn, tolist([""])), 0) : ""
 }
 
 output "lb_zone_id" {
   description = "Loadbalancer Zone ID"
 
-  value = var.enable_lb ? element(concat(aws_alb.main.*.zone_id, list("")), 0) : ""
+  value = var.enable_lb ? element(concat(aws_alb.main.*.zone_id, tolist([""])), 0) : ""
 }
 
 output "task_definition_arn" {
@@ -36,17 +36,17 @@ output "task_definition_arn" {
 output "lb_target_group_arn" {
   description = "Loadbalancer Target Group ARN"
 
-  value = var.enable_lb ? element(concat(aws_alb_target_group.main.*.arn, list("")), 0) : ""
+  value = var.enable_lb ? element(concat(aws_alb_target_group.main.*.arn, tolist([""])), 0) : ""
 }
 
 output "lb_listener_arn" {
   description = "Loadbalancer Listener ARN"
 
-  value = var.enable_lb ? element(concat(aws_alb_listener.main.*.arn, list("")), 0) : ""
+  value = var.enable_lb ? element(concat(aws_alb_listener.main.*.arn, tolist([""])), 0) : ""
 }
 
 output "lb_security_group_id" {
   description = "Loadbalancer Security Group id"
 
-  value = var.enable_lb ? element(concat(aws_security_group.alb_sg.*.id, list("")), 0) : ""
+  value = var.enable_lb ? element(concat(aws_security_group.alb_sg.*.id, tolist([""])), 0) : ""
 }
